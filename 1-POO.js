e7ffc1cbeb6dc5ffbdfd7b411408ba9c3b7e975d
DenisVasconcellos <denis_mvasconcellos@hotmail.com>
2021-10-01 18:35:13 -0300

    complementando orientacao a objetos

--- 1-POO.js
+++ 1-POO.js
@@ -1,7 +1,20 @@
-function Pessoa(name){ 
+/* function Pessoa(name){ 
     this.name = name;
 }
 
 const p = new Pessoa('Guilherme')
 
-console.log(p)
+console.log(p) */
+
+function Animal(qtdePatas){
+    this.qtdePatas = qtdePatas;
+}
+
+function Cachorro(morde){
+    Animal.call(this, 4);
+    this.morde = morde;
+}
+
+const pug = new Cachorro(false);
+
+console.log(pug);
